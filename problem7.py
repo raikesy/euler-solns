27ef0be89642bd2e8a0086fdea60cbf01ddbb31b
James Raikes <James.Raikes@Shield-group.com>
2016-07-12 13:36:46 +0100

    p7v1 complete but too slow

--- problem7.py
+++ problem7.py
@@ -6,21 +6,23 @@
 # Divide by 2, 3, NOT 4, 5, NOT 6, 7 etc
 
 def is_prime(test_value):
-    '''v1, pure brute force'''
+    '''v1, pure brute force: too slow'''
+    if test_value == 1: return False
     for i in range(2,test_value):
-        if test_value % i == 0:
+        if not test_value % i:
             return False
     return True
 
 
 def main():
-    prime_no = 10
+    '''v1, tests all numbers for primality'''
+    prime_no = 1000
     prime_count = 0
-    test_number = 1
+    test_number = 0
     while prime_count < prime_no:
+        test_number += 1
         if is_prime(test_number):
             prime_count += 1
-        test_number += 1
     print(test_number)
 
 if __name__ == "__main__":
