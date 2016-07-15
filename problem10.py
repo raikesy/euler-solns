44577625f7fcb0d8a50cd9314101f51a17fe2150
James Raikes <James.Raikes@Shield-group.com>
2016-07-15 11:34:33 +0100

    Problem 10 passed, profile script started

--- problem10.py
+++ problem10.py
@@ -12,10 +12,20 @@ def sieve(upper):
         int_list = [x for x in int_list if x % prime_list[-1]]
     return prime_list
 
+def sieve2(upper):
+    '''Version of sieve that follows Wikipedia's pseudocode
 
+    Avoids using list comprehension which is apparently quite slow
+    Further optimisation may be possible using numpy arrays'''
+    A = [True] * (upper - 2)    # Don't need bool for 1 or upper'
+    for i in range(2, int(upper ** 0.5) + 1):
+        if A[i - 2]:
+            for j in range(i ** 2, upper, i):
+                A[j - 2] = False
+    return [x + 2 for x in range(len(A)) if A[x]]
 
 def main():
-    print(sum(sieve(60000)))
+    print(sum(sieve2(2000000)))
 
 
 if __name__ == "__main__":
