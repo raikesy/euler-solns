d07c7534ed6ad18c75f075b9b4fcb3a3aec55145
James Raikes <James.Raikes@Shield-group.com>
2016-07-15 10:15:17 +0100

    Changed P10 to use a basic sieve Still too slow

--- problem10.py
+++ problem10.py
@@ -1,23 +1,21 @@
 '''Project Euler Problem 10
 
-Find the sum of every prime under 2 million'''
-import problem7 as p7
+Find the sum of every prime under 2 million
+v2, use a basic sieve of Eratosthenes'''
 
-def prime_sum(upper):
-    i = 1
-    p_sum = 0
-    current_prime = 0
-    while True:
-        current_prime = p7.prime_generator(i)
-        if current_prime >= upper:
-            break
-        p_sum += current_prime
-        i += 1
-    return p_sum
+def sieve(upper):
+    '''Return every prime below a given value'''
+    prime_list = []
+    int_list = list(range(2,upper))
+    while int_list:
+        prime_list.append(int_list[0])
+        int_list = [x for x in int_list if x % prime_list[-1]]
+    return prime_list
+
 
 
 def main():
-    print(prime_sum(10000))
+    print(sum(sieve(60000)))
 
 
 if __name__ == "__main__":
